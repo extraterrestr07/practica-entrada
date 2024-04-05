9cb76241178448f5527e0ec879c4a89245ed95c9
Rodrigo Viza <rodrigoestefanovizacuti@gmail.com>
2024-04-04 23:32:34 -0500

    Implementacion de clase Desarrollador

--- Gestion-Empleados/src/Desarrollador.java
+++ Gestion-Empleados/src/Desarrollador.java
@@ -9,16 +9,15 @@ public class Desarrollador extends Base_Empleado implements EvaluacionDesempeno{
         this.lenguajes = lenguajes;
     }
 
-    
-
     @Override
     double calcularBono() {
-        if(lenguajes.size()<4)
-            return 
-        return 0;
+        if(evaluarDesempeno().equals("BUENO"))
+            return salario;
+        else if(evaluarDesempeno().equals("REGULAR"))
+            return salario/2;
+        else
+            return 0;
     }
-
-
 
     @Override
     void mostrarDetalles() {
@@ -32,21 +31,12 @@ public class Desarrollador extends Base_Empleado implements EvaluacionDesempeno{
 
     @Override
     public String evaluarDesempeno() {
-        Scanner entrada=new Scanner(System.in);
-        int valorEmpleado=-1;
-        do{
-        System.out.println("Ingrese la cantidad de estrellas del empleado de 1 a 10: ");
-        valorEmpleado=entrada.nextInt();
-        }
-        while(valorEmpleado>10||valorEmpleado<0);
-
-        if (len>=7) 
+        if (lenguajes.size()>=7) 
             return "BUENO";
-        else if (valorEmpleado<=4) 
+        else if (lenguajes.size()>=4) 
             return "REGULAR";
         else
             return "NADA";
-
     }
 
     
